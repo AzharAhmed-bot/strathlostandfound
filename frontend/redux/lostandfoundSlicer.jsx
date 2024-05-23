4203473cbd5eb2da4fc3e47873f431e0b13c44b0
azhar <azharahmedtakoy4444@gmail.com>
2024-05-23 12:50:56 +0300

    ♻️ code clean up exercise

--- frontend/redux/lostandfoundSlicer.jsx
+++ frontend/redux/lostandfoundSlicer.jsx
@@ -12,24 +12,7 @@ export const fetchLostAndFound = createAsyncThunk(
     return response.json();
   }
 );
-export const postQuestion = createAsyncThunk(
-  "lostandfound/postQuestion",
-  async ({question,endpoint}) => {
 
-    const response = await fetch(`http://localhost:5000/${endpoint}`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ review: question }),
-    });
-    if (response.ok) {
-      return response.json();
-    } else {
-      throw new Error('Failed to post question');
-    }
-  }
-);
 
 const initialState = {
   isLoading: false,
@@ -53,21 +36,8 @@ const lostAndFoundSlice = createSlice({
       state.isLoading = false;
       state.error = true;
     });
-    builder.addCase(postQuestion.pending, (state) => {
-      state.isLoading = true;
-    });
-    builder.addCase(postQuestion.fulfilled, (state, action) => {
-      state.isLoading = false;
-      state.data = action.payload;
-    });
-    builder.addCase(postQuestion.rejected, (state) => {
-      state.isLoading = false;
-      state.error = true;
-    });
   },
 });
 
-
-export const { rejectClaim } = lostAndFoundSlice.actions;
 
 export default lostAndFoundSlice.reducer;
